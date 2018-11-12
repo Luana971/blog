f19f6e4a2ac0a60c2fd5b61aa183886555701f70
Lüana Lorthios <luanalorthios@windowslive.com>
2018-11-12 11:28:50 +0100

    ajout home - quete symfony 3

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: luana
+ * Date: 12/11/18
+ * Time: 11:09
+ */
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+
+    public function index()
+    {
+        return $this->render('home.html.twig');
+    }
+}
