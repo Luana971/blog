1b77ca1fd5d78d8affb025b43d4cba5f9927f57b
Lüana Lorthios <luanalorthios@windowslive.com>
2018-11-26 14:55:59 +0100

    modif fixtures slugify

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -18,19 +18,20 @@ use Faker;
 
 class ArticleFixtures extends Fixture implements DependentFixtureInterface
 {
-    public function load(ObjectManager $manager, Slugify $slugify)
+    public function load(ObjectManager $manager)
     {
         for ($i = 0; $i < 50; $i++)
         {
             $faker  =  Faker\Factory::create('fr_FR');
 
+            $slug = new Slugify();
             $article = new Article();
             $article->setTitle(mb_strtolower($faker->sentence));
             $article->setContent($faker->text);
 
             $manager->persist($article);
             $article->setCategory($this->getReference('category_' . rand(0, 4)));
-            $article->setSlug($slugify->generate($article->getTitle()));
+            $article->setSlug($slug->generate($article->getTitle()));
         }
         $manager->flush();
     }
